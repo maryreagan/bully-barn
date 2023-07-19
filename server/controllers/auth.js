9cd4df30ab4daf24df5f0e3ae98fa82fce6b9a2c
Collin Letourneau <collin.r.letourneau@gmail.com>
2023-07-19 13:33:58 -0400

    pulled from develop

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,9 +3,9 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const SALT = Number(process.env.SALT);
 const jwt = require("jsonwebtoken");
-const { isValidPwd, isValidEmail }  = require("../helpers/validate");
-const { sendValidationEmail } = require("../helpers/validationEmail")
-const validationLink = "!!!(Validation link here)"
+const { isValidPwd, isValidEmail } = require("../helpers/validate");
+const { sendValidationEmail } = require("../helpers/validationEmail");
+const validationLink = "!!!(Validation link here)";
 const JWT_KEY = process.env.JWT_KEY;
 
 // Register route
@@ -20,15 +20,16 @@ router.post("/register", async (req, res) => {
         }
 
         if (!isValidEmail(email)) {
-          return res.status(400).json({
-            message: 'Please enter a valid email address'
-          })
+            return res.status(400).json({
+                message: "Please enter a valid email address",
+            });
         }
 
         if (!isValidPwd(password)) {
-          return res.status(400).json({
-            message: 'Password must be at least 7 characters with at least one letter and one number'
-          })
+            return res.status(400).json({
+                message:
+                    "Password must be at least 7 characters with at least one letter and one number",
+            });
         }
 
         const hashedPwd = await bcrypt.hash(password, SALT);
@@ -46,7 +47,7 @@ router.post("/register", async (req, res) => {
         }
 
         await createdUser.save();
-        await sendValidationEmail(createdUser.email, validationLink)
+        await sendValidationEmail(createdUser.email, validationLink);
 
         const token = jwt.sign({ _id: createdUser._id }, JWT_KEY, {
             expiresIn: 60 * 60 * 24,
