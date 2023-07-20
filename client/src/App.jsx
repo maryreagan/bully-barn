4586122dabd8aff5f28e27681e84a58d40f164df
[Tiffania] <ielenatiffania@gmail.com>
2023-07-20 17:11:50 -0400

    Changes for Stripe Payment Front End

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+import AdoptionFeePage from "./components/Adoption-Fee/AdoptionFee"
+import PaymentStatusPage from './components/Adoption-Fee/PaymentStatusPage'
 
 function App() {
 
 
   return (
     <>
-
+      <Router>
+        <Routes>
+          <Route path="/adoption-fee" element={<AdoptionFeePage />} />
+          <Route path="/payment-status" element={<PaymentStatusPage />} />
+        </Routes>
+      </Router>
     </>
   )
 }
