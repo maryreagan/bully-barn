fc0cf3eea9cb3f103879e4fb4a1bf1d1436e51ac
424ngel <angel.aguilarestra@gmail.com>
2023-07-25 14:53:14 -0400

    Completed status banners

--- client/src/components/Dog/Dog.jsx
+++ client/src/components/Dog/Dog.jsx
@@ -131,11 +131,25 @@ function Dog() {
    setFilteredDogs(filtered)
   }, [dogs, filters])
 
+  const bannerSwitch = (dog) => {
+    if (dog.adoptionStatus === 'pending') return 'pending-card'
+    if (dog.adoptionStatus === 'adopted') return 'adopted-card'
+    if (dog.sponsorshipStatus) return 'sponsored-card' 
+    return false
+  }
+
+  const displayBanner = (dog) => {
+    if (dog.adoptionStatus === "pending") return 'PENDING'
+    if (dog.adoptionStatus === 'adopted') return 'ADOPTED'
+    if (dog.sponsorshipStatus) return 'SPONSORED!' 
+  }
+  
+
   const displayDogs = () => {
     return (
       dogs && filteredDogs.map((dog) => ( 
-        <div id='dog-card' className={dog.sponsorshipStatus ? 'sponsored-card' : 'none' } key={dog._id} onClick={() => displayDogDetails(dog)}>
-          {dog.sponsorshipStatus && <div id='sponsored-dog'>SPONSORED!</div>}
+        <div id='dog-card' className={bannerSwitch(dog)} key={dog._id} onClick={() => displayDogDetails(dog)}>
+          {bannerSwitch(dog) && <div id={`head-${bannerSwitch(dog)}`}>{displayBanner(dog)}</div>}
           <div id='img-container'>
             <img src={dog.image} alt={dog.name} />
           </div>
